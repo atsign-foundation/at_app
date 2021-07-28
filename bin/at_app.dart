2d1864cda49b9e8f4135dd3a4d0689cafc4cb40a
XavierChanth <xchanthavong@gmail.com>
2021-07-28 18:52:38 -0400

    fix flutter create throwing null

--- bin/at_app.dart
+++ bin/at_app.dart
@@ -1,16 +1,27 @@
 // @dart = 2.8
 import 'package:at_app/commands/create.dart';
 import 'package:flutter_tools/runner.dart' as runner;
+import 'package:flutter_tools/executable.dart' show LoggerFactory;
 import 'package:flutter_tools/src/runner/flutter_command.dart';
 import 'package:flutter_tools/src/globals.dart' as globals;
 import 'package:flutter_tools/src/cache.dart';
 import 'package:flutter_tools/src/base/platform.dart';
 import 'package:flutter_tools/src/base/user_messages.dart';
+import 'package:flutter_tools/src/base/context.dart';
+import 'package:flutter_tools/src/base/template.dart';
+import 'package:flutter_tools/src/isolated/resident_web_runner.dart';
+import 'package:flutter_tools/src/isolated/mustache_template.dart';
+import 'package:flutter_tools/src/web/web_runner.dart';
+import 'package:flutter_tools/src/resident_runner.dart';
+import 'package:flutter_tools/src/devtools_launcher.dart';
+import 'package:flutter_tools/src/artifacts.dart';
+import 'package:flutter_tools/src/base/logger.dart';
 
 Future<void> main(List<String> args) async {
   final veryVerbose = args.contains('-vv');
   final verbose =
       args.contains('-v') || args.contains('--verbose') || veryVerbose;
+  final prefixedErrors = args.contains('--prefixed-errors');
   // Support the -? Powershell help idiom.
   final powershellHelpIndex = args.indexOf('-?');
   if (powershellHelpIndex != -1) {
@@ -25,6 +36,9 @@ Future<void> main(List<String> args) async {
       (args.length == 1 && verbose);
   final muteCommandLogging = (help || doctor) && !veryVerbose;
   final verboseHelp = help && verbose;
+  final daemon = args.contains('daemon');
+  final runMachine = (args.contains('--machine') && args.contains('run')) ||
+      (args.contains('--machine') && args.contains('attach'));
 
   // Cache.flutterRoot must be set early because other features use it (e.g.
   // enginePath's initializer uses it). This can only work with the real
@@ -43,6 +57,37 @@ Future<void> main(List<String> args) async {
     verbose: verbose,
     muteCommandLogging: muteCommandLogging,
     verboseHelp: verboseHelp,
+    overrides: <Type, Generator>{
+      // The web runner is not supported in google3 because it depends
+      // on dwds.
+      WebRunnerFactory: () => DwdsWebRunnerFactory(),
+      // The mustache dependency is different in google3
+      TemplateRenderer: () => const MustacheTemplateRenderer(),
+      // The devtools launcher is not supported in google3 because it depends on
+      // devtools source code.
+      DevtoolsLauncher: () => DevtoolsServerLauncher(
+            processManager: globals.processManager,
+            pubExecutable:
+                globals.artifacts.getArtifactPath(Artifact.pubExecutable),
+            logger: globals.logger,
+            platform: globals.platform,
+            persistentToolState: globals.persistentToolState,
+          ),
+      Logger: () {
+        final loggerFactory = LoggerFactory(
+          outputPreferences: globals.outputPreferences,
+          terminal: globals.terminal,
+          stdio: globals.stdio,
+        );
+        return loggerFactory.createLogger(
+          daemon: daemon,
+          machine: runMachine,
+          verbose: verbose && !muteCommandLogging,
+          prefixedErrors: prefixedErrors,
+          windows: globals.platform.isWindows,
+        );
+      },
+    },
   );
 }
 
