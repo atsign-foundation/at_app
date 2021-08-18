138dd743c31d0642a60e62739885d3b842bd5e30
XavierChanth <xchanthavong@gmail.com>
2021-08-18 09:55:03 -0400

    Testing brew fix for flutter

--- pub/bin/at_app.dart
+++ pub/bin/at_app.dart
@@ -1,9 +1,12 @@
 // @dart = 2.8
 import 'package:flutter_tools/src/runner/flutter_command.dart';
+import 'package:pub_cache/pub_cache.dart';
 import 'src/executable.dart';
 import 'src/commands/create.dart';
 
 Future<void> main(List<String> args) async {
+  PubCache pc = new PubCache();
+  print('cache => ${pc.location}');
   await run(args, generateCommands);
 }
 
