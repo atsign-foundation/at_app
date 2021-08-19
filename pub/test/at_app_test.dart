c0cf022cf033cd33a96a9af7e12016ef6763d31f
XavierChanth <xchanthavong@gmail.com>
2021-08-19 11:08:27 -0400

    Update template manager tests

--- pub/test/at_app_test.dart
+++ pub/test/at_app_test.dart
@@ -1,29 +1,36 @@
+// @dart=2.8
 import 'package:pub_cache/pub_cache.dart';
-// ignore: import_of_legacy_library_into_null_safe
 import 'package:flutter_tools/src/globals.dart' as globals;
 import 'dart:io';
+import 'package:test/test.dart';
 
 void main() {
-  testTemplateManager();
-}
+  group('Template Manager', () {
+    test('pub cache is found', () {
+      String pubCachePath = PubCache().location.absolute.path;
+      expect(pubCachePath.length > 0, true);
+    });
 
-void testTemplateManager() {
-  PubCache pc = new PubCache();
+    test('at_app is found in pub cache', () {
+      PackageRef latest = PubCache().getLatestVersion('at_app');
+      expect(latest == null, false);
+    });
 
-  String pubCachePath = pc.location.absolute.path;
-  assert(pubCachePath.length > 0);
+    test('main.dart template exists in the pub cache', () {
+      PubCache pc = PubCache();
+      String hostedPubCachePath = globals.fs.path
+          .normalize('${pc.location.absolute.path}/hosted/pub.dartlang.org');
 
-  PackageRef? latest = pc.getLatestVersion('at_app');
+      String templatePath = globals.fs.path.join(
+          hostedPubCachePath,
+          'at_app-${pc.getLatestVersion('at_app')?.version.toString()}',
+          'lib',
+          'templates',
+          'main.dart');
 
-  assert(latest != null);
+      File templateFile = File(templatePath);
 
-  String hostedPubCachePath = globals.fs.path
-      .normalize('${pc.location.absolute.path}/hosted/pub.dartlang.org');
-
-  String templatePath = globals.fs.path.join(hostedPubCachePath,
-      'at_app-${latest?.version.toString()}', 'lib', 'main.dart');
-
-  File templateFile = File(templatePath);
-
-  assert(templateFile.existsSync());
+      expect(templateFile.existsSync(), true);
+    });
+  });
 }
