7063d047073dd01b0f0c9a0ee72dcb7785ec81a6
XavierChanth <xchanthavong@gmail.com>
2021-08-21 11:33:16 -0400

    run dart format

--- lib/src/util/command_status.dart
+++ lib/src/util/command_status.dart
@@ -1,7 +1,3 @@
 /// An enum for the status of the command
 /// The command can be a success, provide a warning, or completely fail.
-enum CommandStatus {
-  success,
-  warning,
-  fail
-}
+enum CommandStatus { success, warning, fail }
