118f710eb15bd1a93a4e4a826e9650f07324bf33
XavierChanth <xchanthavong@gmail.com>
2021-08-25 09:32:24 -0400

    Remove ... after path in output

--- lib/src/commands/create.dart
+++ lib/src/commands/create.dart
@@ -59,9 +59,9 @@ class CreateCommand extends CreateBase {
         path.join(relativeOutputPath, 'lib', 'main.dart');
 
     if (creatingNewProject) {
-      _logger.i('Creating project in $relativeOutputPath...');
+      _logger.i('Creating project in $relativeOutputPath');
     } else {
-      _logger.i('Recreating project in $relativeOutputPath...');
+      _logger.i('Recreating project in $relativeOutputPath');
     }
 
     CommandStatus flutterCreateResult = await super.run();
