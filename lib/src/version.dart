cc31c01696017bf5b33445ded1020c79ec6c0878
XavierChanth <xchanthavong@gmail.com>
2021-09-10 16:04:40 -0400

    Manually set the version in version.dart

--- lib/src/version.dart
+++ lib/src/version.dart
@@ -1,2 +1,10 @@
-// Generated code. Do not modify.
-const packageVersion = '1.0.2';
+import 'package:pub_semver/pub_semver.dart';
+
+/// Current version of this package
+final packageVersion = Version(1, 0, 2);
+
+/// Name of the package containing the templates
+final templatePackageName = 'at_app_flutter';
+
+/// Version of the package to use for templates
+final templatePackageVersion = Version(0, 2, 0);
