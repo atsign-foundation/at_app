577779f795e29c3d053cca8e53a1a11f57f31dd9
XavierChanth <xchanthavong@gmail.com>
2021-09-30 14:44:51 -0400

    bump at_app flutter version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.1.0-rc+3
+
+- at_app now uses at_app_flutter version 1.0.0-rc
+
 ## 1.1.0-rc+2
 
 - at_app now uses .dart_tool/package_config.json to locate the template files contained in at_app_flutter
--- lib/src/version.dart
+++ lib/src/version.dart
@@ -1,10 +1,10 @@
 import 'package:pub_semver/pub_semver.dart';
 
 /// Current version of this package
-final packageVersion = Version(1, 1, 0, pre: 'rc', build: '2');
+final packageVersion = Version(1, 1, 0, pre: 'rc', build: '3');
 
 /// Name of the package containing the templates
 final templatePackageName = 'at_app_flutter';
 
 /// Version of the package to use for templates
-final templatePackageVersion = Version(0, 2, 0, pre: 'rc');
+final templatePackageVersion = Version(1, 0, 0, pre: 'rc');
