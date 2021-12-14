66eb00d6e3902ff5fa3b572f83c8682147576490
XavierChanth <xchanthavong@gmail.com>
2021-12-14 10:47:21 -0500

    Initialize at_app_templates

---
+++ at_app_templates/lib/at_app_templates.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library at_app_templates;
+
+export 'src/at_app_templates_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
