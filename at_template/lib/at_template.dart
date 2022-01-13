be7cedf0bb1e577acd72d562f2cc3045e2369c8e
XavierChanth <xchanthavong@gmail.com>
2022-01-13 14:41:48 -0500

    Export the library

---
+++ at_template/lib/at_template.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library at_template;
+
+export 'src/vars/vars.dart';
+export 'src/models/models.dart';
+export 'src/bundles/bundles.dart';
