c4ae0ac92d8266b43ceb33608a42b55c2e927854
XavierChanth <xchanthavong@gmail.com>
2022-01-18 16:43:03 -0500

    Remove unused models and services

--- at_app/lib/src/services/template_service.dart
+++ at_app/lib/src/services/template_service.dart
@@ -1,10 +1,19 @@
-import 'package:at_app/src/constants/templates.dart';
+import 'package:at_app/src/constants/templates.dart' as c;
 import 'package:at_app/src/models/at_app_template.dart';
 
 class TemplateService {
-  static AtAppTemplate getTemplate(String name) => templates.firstWhere((element) => element.name == name);
+  static AtAppTemplate getTemplate(String name) => c.templates.firstWhere((element) => element.name == name);
 
-  static AtAppTemplate getSample(String name) => samples.firstWhere((element) => element.name == name);
+  static Map<String, String> get templateNames =>
+      c.templates.map((e) => MapEntry(e.name, e.description)) as Map<String, String>;
 
-  static AtAppTemplate getDemo(String name) => demos.firstWhere((element) => element.name == name);
+  static AtAppTemplate getSample(String name) => c.samples.firstWhere((element) => element.name == name);
+
+  static Map<String, String> get sampleNames =>
+      c.samples.map((e) => MapEntry(e.name, e.description)) as Map<String, String>;
+
+  static AtAppTemplate getDemo(String name) => c.demos.firstWhere((element) => element.name == name);
+
+  static Map<String, String> get demoNames =>
+      c.demos.map((e) => MapEntry(e.name, e.description)) as Map<String, String>;
 }
