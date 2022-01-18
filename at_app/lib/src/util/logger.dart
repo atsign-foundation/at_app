d69af67986ca5903e0d386bd9b8cd7a8eda2bcf2
XavierChanth <xchanthavong@gmail.com>
2022-01-18 15:38:56 -0500

    Change info logger to white

--- at_app/lib/src/util/logger.dart
+++ at_app/lib/src/util/logger.dart
@@ -21,7 +21,7 @@ class _Printer extends LogPrinter {
   AnsiColor _color(Level level) => {
         Level.verbose: AnsiColor.fg(AnsiColor.grey(0.5)),
         Level.debug: AnsiColor.none(),
-        Level.info: AnsiColor.fg(12),
+        Level.info: AnsiColor.fg(AnsiColor.grey(1)),
         Level.warning: AnsiColor.fg(208),
         Level.error: AnsiColor.fg(196),
         Level.wtf: AnsiColor.fg(199),
