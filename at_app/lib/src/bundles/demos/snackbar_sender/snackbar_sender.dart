4f39ce61a6290ea46995b66d0f1565de3e2110c9
XavierChanth <xchanthavong@gmail.com>
2022-01-26 09:29:16 -0500

    rebuild templates

--- at_app/lib/src/bundles/demos/snackbar_sender/snackbar_sender.dart
+++ at_app/lib/src/bundles/demos/snackbar_sender/snackbar_sender.dart
@@ -4,6 +4,7 @@
 
 import 'package:at_template/at_template.dart';
 import 'package:at_app/src/models/at_app_template.dart';
+import 'package:pub_semver/pub_semver.dart';
 
 import 'snackbar_sender_template_bundle.dart';
 
@@ -15,7 +16,10 @@ final snackbarSenderTemplate = AtAppTemplate(
   vars: AtTemplateVars(
     includeBundles: {'snackbar_sender'},
     dependencies: ["at_client_mobile: ^3.0.3","at_onboarding_flutter: ^3.0.1","at_utils: ^3.0.0","path_provider: ^2.0.5","flutter_dotenv: ^5.0.2","at_app_flutter: "],
-    flutterConfig: [],
+    kotlinVersion: Version.parse('1.4.31'),
+    flutterConfig: ["assets:","  - .env"],
   ),
+  overrideEnv: true,
+  env: {"NAMESPACE":"fourballcorporate9","API_KEY":"477b-876u-bcez-c42z-6a3d"},
   bundles: [BaseTemplateBundle(), AndroidTemplateBundle(), IosTemplateBundle(), SnackbarSenderTemplateBundle()],
 );
--- at_app/lib/src/bundles/templates/app/app.dart
+++ at_app/lib/src/bundles/templates/app/app.dart
@@ -15,7 +15,8 @@ final appTemplate = AtAppTemplate(
   vars: AtTemplateVars(
     includeBundles: {'app'},
     dependencies: ["at_client_mobile: ^3.0.3","at_onboarding_flutter: ^3.0.1","at_utils: ^3.0.0","path_provider: ^2.0.5","flutter_dotenv: ^5.0.2","at_app_flutter: "],
-    flutterConfig: [],
+    enableR8: true,
+    flutterConfig: ["assets:","  - .env"],
   ),
   bundles: [BaseTemplateBundle(), AndroidTemplateBundle(), IosTemplateBundle(), AppTemplateBundle()],
 );
