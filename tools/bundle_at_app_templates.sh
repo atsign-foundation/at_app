251b681c092bb73efc95878f4bf98094dd6472b5
XavierChanth <xchanthavong@gmail.com>
2022-01-26 09:04:38 -0500

    Clean up bundler_at_app_templates.sh script

--- tools/bundle_at_app_templates.sh
+++ tools/bundle_at_app_templates.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 TOOL_PATH="${BASH_SOURCE%/*}"
 BASE_PATH="$TOOL_PATH/../templates"
-BRICK_PATH="$BASE_PATH/lib/src"
+BRICK_PATH="$BASE_PATH/lib"
 OUTPUT_PATH="$TOOL_PATH/../at_app"
 
 rm "$OUTPUT_PATH/lib/src/bundles/bundles.dart"
 touch "$OUTPUT_PATH/lib/src/bundles/bundles.dart"
 for x in "$BRICK_PATH"/*;
-do
-  echo "export '$(basename "$x")/$(basename "$x").dart';" >> "$OUTPUT_PATH/lib/src/bundles/bundles.dart"
-  rm "$OUTPUT_PATH/lib/src/bundles/$(basename "$x")/$(basename "$x").dart"
-  touch "$OUTPUT_PATH/lib/src/bundles/$(basename "$x")/$(basename "$x").dart"
+  do
+  xbase=$(basename "$x")
+  echo "export '$xbase/$xbase.dart';" >> "$OUTPUT_PATH/lib/src/bundles/bundles.dart"
+  rm "$OUTPUT_PATH/lib/src/bundles/$xbase/$xbase.dart"
+  touch "$OUTPUT_PATH/lib/src/bundles/$xbase/$xbase.dart"
   for y in "$x"/*;
   do
-    echo "export '$(basename "$y")/$(basename "$y").dart';" >> "$OUTPUT_PATH/lib/src/bundles/$(basename "$x")/$(basename "$x").dart"
-    dart run "$TOOL_PATH/template_bundler/bin/main.dart" bundle -o "$OUTPUT_PATH/lib/src/bundles/$(basename "$x")/$(basename "$y")" "$y";
+    ybase=$(basename "$y")
+    echo "export '$ybase/$ybase.dart';" >> "$OUTPUT_PATH/lib/src/bundles/$(basename "$x")/$(basename "$x").dart"
+    dart run "$TOOL_PATH/template_bundler/bin/main.dart" bundle -o "$OUTPUT_PATH/lib/src/bundles/$xbase/$ybase" "$y";
   done
 done
