dd52b8eb02046fadfd2378b3945513f0504a26d8
XavierChanth <xchanthavong@gmail.com>
2022-02-12 07:54:56 -0500

    Cleanup build scripts

---
+++ tools/build_all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+TOOL_PATH="${BASH_SOURCE%/*}"
+
+# cd to project root
+cd "$TOOL_PATH/.." || exit 1
+
+# Run the builds without format and analuze
+dart pub global run melos run build:at_template -- --no-format <<< n;
+dart pub global run melos run build:templates -- --no-format  <<< n;
+dart pub global run melos run build:generated <<< n;
+
+# Format and analyze manually, so that the output appears at the end
+dart format -l 120 at_template/
+dart format -l 120 at_app/
+
+dart analyze at_template/
+dart analyze at_app/
--- tools/full_build_at_app_templates.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-TOOL_PATH="${BASH_SOURCE%/*}"
-
-"$TOOL_PATH"/bundle_at_app_templates.sh
-"$TOOL_PATH"/format_and_analyze.sh "at_app"
