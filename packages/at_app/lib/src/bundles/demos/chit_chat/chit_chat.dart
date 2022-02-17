70e6ef7ddbf48a24f69b82cd996d0a50b89cfef2
XavierChanth <xchanthavong@gmail.com>
2022-02-17 15:27:19 -0500

    Rebuild the templates

--- packages/at_app/lib/src/bundles/demos/chit_chat/chit_chat.dart
+++ packages/at_app/lib/src/bundles/demos/chit_chat/chit_chat.dart
@@ -17,15 +17,14 @@ final chitChatTemplate = AtAppTemplate(
     includeBundles: {'chit_chat'},
     dependencies: [
       "at_client_mobile: ^3.0.3",
-      "at_onboarding_flutter: ^3.0.1",
       "at_utils: ^3.0.0",
       "path_provider: ^2.0.5",
       "flutter_dotenv: ^5.0.2",
       "at_chat_flutter: ^3.0.3",
       "at_contacts_flutter: ^4.0.0",
-      "at_app_flutter: "
+      "at_app_flutter: ^5.0.0"
     ],
-    kotlinVersion: Version.parse('1.4.31'),
+    kotlinVersion: Version.parse('1.5.32'),
     minSdkVersion: '23',
     flutterConfig: ["assets:", "  - .env"],
   ),
