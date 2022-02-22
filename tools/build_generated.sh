fa6c91dc90e394ad1d15dd4af805b612358c38cb
XavierChanth <xchanthavong@gmail.com>
2022-02-22 18:57:26 -0500

    Fix build_generated output

--- tools/build_generated.sh
+++ tools/build_generated.sh
@@ -21,6 +21,7 @@ rm -rf "${OUTPUT_PATH:?}/"*
 echo "READING TEMPLATES"
 TEMPLATES="$(dart run "$MAIN_FILE" create --list-templates | cut -f1 -d '|' | sed -e '1,3d' | tr '\n' ' ')"
 echo "BUILDING TEMPLATES"
+echo "$TEMPLATES"
 build_apps t "$TEMPLATES"
 
 echo "READING SAMPLES"
@@ -32,4 +33,5 @@ build_apps s "$SAMPLES"
 echo "READING DEMOS"
 DEMOS="$(dart run "$MAIN_FILE" create --list-demos | cut -f1 -d '|' | sed -e '1,3d' | tr '\n' ' ')"
 echo "BUILDING DEMOS"
+echo "$DEMOS"
 build_apps d "$DEMOS"
