98c32ee38fdcc929ee32248e9b6756fec74595c4
XavierChanth <xchanthavong@gmail.com>
2022-02-23 19:51:36 -0500

    Update script references

--- tools/build_all.sh
+++ tools/build_all.sh
@@ -9,8 +9,11 @@ fi
 # cd to project root
 cd "$MELOS_ROOT_PATH" || exit 1
 
+# First build the bundler
+dart pub global run melos run build:bundler -- --no-format;
+
 # Run the builds without format and analyze
-dart pub global run melos run build:at_app_create -- --no-format;
+dart pub global run melos run build:create -- --no-format;
 dart pub global run melos run build:templates -- --no-format;
 
 # Build at_app
@@ -20,9 +23,12 @@ dart pub global run melos run build:at_app;
 dart pub global run melos run build:generated;
 
 # Format and analyze manually, so that the output appears at the end
+dart format -l 120 packages/at_app_bundler/
 dart format -l 120 packages/at_app_create/
 dart format -l 120 packages/at_app/
 
+dart analyze packages/at_app_flutter/
+dart analyze packages/at_app_bundler/
 dart analyze packages/at_app_create/
 dart analyze packages/at_app/
 dart analyze build/
