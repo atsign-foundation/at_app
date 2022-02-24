c89b3a83c0c499482d36e714cfb6ea8dafd3daf0
XavierChanth <xchanthavong@gmail.com>
2022-02-23 19:51:19 -0500

    Add a build script for the bundler

---
+++ tools/build_bundler.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+BUNDLER_PATH="$MELOS_ROOT_PATH/packages/at_app_bundler"
+BRICK_PATH="$BUNDLER_PATH/bricks"
+
+# ARGS
+FORMAT=1
+
+# ARG PARSER
+while [ $# -gt 0 ];
+do
+  case "$1" in
+    --no-format)
+      FORMAT=0
+    ;;
+  esac
+  shift
+done
+
+# Build the mason bricks in at_app_create and output them into at_app
+for BRICK in "$BRICK_PATH"/*;
+do
+  dart pub global run mason_cli:mason bundle -t dart -o "$BUNDLER_PATH/lib/src/bundles/$(basename "$BRICK")" "$BRICK";
+done
+
+# Format and analyze
+if [ $FORMAT -gt 0 ] ;
+then
+# Cannot format at_app_create directory directly
+  # it will non-zero exit due to the dart templates in the bricks directory
+  dart format -l 120 "$MELOS_ROOT_PATH/packages/at_app_bundler/lib";
+  dart format -l 120 "$MELOS_ROOT_PATH/packages/at_app_bundler/bin";
+  dart analyze "$MELOS_ROOT_PATH/packages/at_app_bundler";
+fi
