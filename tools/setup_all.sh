2e88e83f96429530571f167f28912dd246b2df0a
XavierChanth <xchanthavong@gmail.com>
2022-02-24 07:42:15 -0500

    Add setup script, to make sure all tools are installed correctly.

---
+++ tools/setup_all.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ "$GITHUB_ACTION" ];
+then
+  echo "ERROR: This script should only be run locally, do not use for github actions."
+  exit 1
+fi
+
+dart pub global run melos run setup:mason;
---
+++ tools/setup_mason.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+dart pub global activate mason_cli 0.1.0-dev.12
