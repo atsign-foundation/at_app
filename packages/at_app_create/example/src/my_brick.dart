07198bc8b78c3b18ee433dc3c83166f278798220
XavierChanth <xchanthavong@gmail.com>
2022-02-24 19:53:34 -0500

    Remove old example folder

--- packages/at_app_create/example/src/my_brick.dart
+++
@@ -1,9 +0,0 @@
-import 'package:at_app_create/at_app_create.dart';
-
-import 'my_brick_bundle.dart';
-
-class MyBrickVars extends AtTemplateVars {}
-
-class MyBrick extends AtTemplateBundle<MyBrickVars> {
-  MyBrick() : super(myBrickBundle);
-}
