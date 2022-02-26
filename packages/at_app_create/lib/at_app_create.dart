e0478b9fd9af03cc287b00a905541ea2325ddba6
XavierChanth <xchanthavong@gmail.com>
2022-02-25 19:08:13 -0500

    Migrate and cleanup at_app_create models

--- packages/at_app_create/lib/at_app_create.dart
+++ packages/at_app_create/lib/at_app_create.dart
@@ -1,7 +1,2 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library at_app_create;
-
 export 'src/models/models.dart';
 export 'src/bundles/bundles.dart';
---
+++ packages/at_app_create/lib/src/constants/platforms.dart
@@ -0,0 +1,27 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+const List<String> defaultAvailablePlatforms = <String>[
+  'ios',
+  'android',
+  'windows',
+  'linux',
+  'macos',
+  'web',
+];
+
+// Copyright 2014 The Flutter Authors. All rights reserved.
+const List<String> defaultAllCreatePlatforms = <String>[
+  'ios',
+  'android',
+  'windows',
+  'linux',
+  'macos',
+  'web',
+  'winuwp',
+];
+
+// Copyright 2014 The Flutter Authors. All rights reserved.
+const String defaultPlatformHelp = 'The platforms supported by this project. '
+    'Platform folders (e.g. android/) will be generated in the target project. '
+    'This argument only works when "--template" is set to app or plugin. '
+    'When adding platforms to a plugin project, the pubspec.yaml will be updated with the requested platform. '
+    'Adding desktop platforms requires the corresponding desktop config setting to be enabled.';
