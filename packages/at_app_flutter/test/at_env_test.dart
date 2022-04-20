2fcb598fffa9fbbeffdf09f1e45a69b8e6c71b41
XavierChanth <xchanthavong@gmail.com>
2022-04-20 18:49:15 -0400

    Unit tests for at_app_flutter

---
+++ packages/at_app_flutter/test/at_env_test.dart
@@ -0,0 +1,31 @@
+import 'package:at_app_flutter/src/at_env.dart';
+import 'package:at_onboarding_flutter/at_onboarding_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+class MockDotEnv extends Mock implements DotEnv {}
+
+void main() {
+  group('AtEnv Test', () {
+    setUpAll(() {
+      AtEnv.dotenvInstance = MockDotEnv();
+    });
+
+    test("Get rootDomain", () {
+      when(AtEnv.dotenvInstance.maybeGet("ROOT_DOMAIN")).thenReturn("Mock Root Domain");
+      expect(AtEnv.rootDomain, "Mock Root Domain");
+    });
+
+    test("Get appNamespace", () {
+      when(AtEnv.dotenvInstance.maybeGet("NAMESPACE")).thenReturn("Mock Namespace");
+      expect(AtEnv.appNamespace, "Mock Namespace");
+    });
+
+    test("Get appApiKey", () {
+      when(AtEnv.dotenvInstance.maybeGet("API_KEY")).thenReturn("Mock Api Key");
+      expect(AtEnv.appApiKey, "Mock Api Key");
+      expect(AtEnv.rootEnvironment, RootEnvironment.Production);
+    });
+  });
+}
