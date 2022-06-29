958d3fdd5b1a5cb2e30e6dc7288f6c188830b4ef
CemYilmaz <cemyilmaz463@gmail.com>
2022-06-29 20:09:00 +0300

    Revert "Added at_contact_group_flutter sample"
    This reverts commit b7039efc4ea78fa809ea671344d78138c83253c4.

--- packages/at_app/lib/src/bundles/samples/samples.dart
+++ packages/at_app/lib/src/bundles/samples/samples.dart
@@ -1,5 +1,4 @@
 export 'at_backupkey_flutter/at_backupkey_flutter.dart';
 export 'at_chat_flutter/at_chat_flutter.dart';
 export 'at_contacts_flutter/at_contacts_flutter.dart';
-export 'at_contacts_group_flutter/at_contacts_group_flutter.dart';
 export 'at_onboarding_flutter/at_onboarding_flutter.dart';
