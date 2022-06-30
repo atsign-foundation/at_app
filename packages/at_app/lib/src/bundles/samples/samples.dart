82e3bbd03aeb96457f53a9b26d1968ea5c91efdd
CemYilmaz <cemyilmaz463@gmail.com>
2022-06-30 23:57:20 +0300

    at_events_flutter sample added

--- packages/at_app/lib/src/bundles/samples/samples.dart
+++ packages/at_app/lib/src/bundles/samples/samples.dart
@@ -2,4 +2,5 @@ export 'at_backupkey_flutter/at_backupkey_flutter.dart';
 export 'at_chat_flutter/at_chat_flutter.dart';
 export 'at_contacts_flutter/at_contacts_flutter.dart';
 export 'at_contacts_group_flutter/at_contacts_group_flutter.dart';
+export 'at_events_flutter/at_events_flutter.dart';
 export 'at_onboarding_flutter/at_onboarding_flutter.dart';
