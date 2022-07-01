2f05024984c31f1544dce2a36f86eb9bc3b4c956
CemYilmaz <cemyilmaz463@gmail.com>
2022-07-02 00:32:12 +0300

    feat: Added at_sync_ui_flutter sample

--- packages/at_app/lib/src/templates.dart
+++ packages/at_app/lib/src/templates.dart
@@ -14,6 +14,7 @@ final List<AtAppTemplate> samples = [
   atContactsGroupFlutterTemplate,
   atInvitationFlutterTemplate,
   atLocationFlutterTemplate,
+  atSyncUiFlutterTemplate,
 ];
 
 final List<AtAppTemplate> demos = [
