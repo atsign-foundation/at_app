1858ea3bbd701847eeef9eb024287a0c369f610d
xavierchanth <xchanthavong@gmail.com>
2023-05-25 13:49:47 -0400

    chore: remove samples, and -t flag

--- packages/at_app/lib/src/templates.dart
+++ packages/at_app/lib/src/templates.dart
@@ -2,23 +2,7 @@ import 'package:at_app_create/at_app_create.dart' show AtAppTemplate;
 
 import 'bundles/bundles.dart';
 
-final List<AtAppTemplate> templates = [
-  appTemplate,
-];
-
-final List<AtAppTemplate> samples = [
-  atOnboardingFlutterTemplate,
-  atContactsFlutterTemplate,
-  atBackupkeyFlutterTemplate,
-  atChatFlutterTemplate,
-  atContactsGroupFlutterTemplate,
-  atEventsFlutterTemplate,
-  atInvitationFlutterTemplate,
-  atLocationFlutterTemplate,
-  atNotifyFlutterTemplate,
-  atSyncUiFlutterTemplate,
-  atThemeFlutterTemplate,
-];
+AtAppTemplate mainTemplate = appTemplate;
 
 final List<AtAppTemplate> demos = [
   snackbarTemplate,
